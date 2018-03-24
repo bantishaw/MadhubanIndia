55be8695f55c89d2917582bf1cb351415808b011
bantishaw <banti.shaw@outlook.com>
2018-03-24 18:07:03 +0530

    Added changes for my order page

--- src/pages/your-orders/your-orders.ts
+++ src/pages/your-orders/your-orders.ts
@@ -14,12 +14,83 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'your-orders.html',
 })
 export class YourOrdersPage {
+  ordersObject: any[];
+  cardItems: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad YourOrdersPage');
-  }
 
+    this.ordersObject = [{
+      "_id": "1",
+      "reference_id": "2",
+      "reference_email": "banti.shaw@outlook.com",
+      "myOrders": [
+        {
+          "order_id": "#1234567890",
+          "date_of_order": "24th march,2018",
+          "total_amount": "1800",
+          "fruits_order_descriptiion": [
+            {
+              "fruit_name": "apple",
+              "number_of_kg_bought": "20",
+              "price_per_kg": "60"
+            },
+            {
+              "fruit_name": "banana",
+              "number_of_kg_bought": "10",
+              "price_per_kg": "60"
+            }
+          ],
+          "OrderStatus": "Delivered",
+          "OrderStatusDescription": "Your order is succssfully delivered"
+        },
+        {
+          "order_id": "#9234567890",
+          "date_of_order": "27th march,2018",
+          "total_amount": "1800",
+          "fruits_order_descriptiion": [
+            {
+              "fruit_name": "kela",
+              "number_of_kg_bought": "20",
+              "price_per_kg": "60"
+            }
+          ],
+          "OrderStatus": "Pending",
+          "OrderStatusDescription": "Your order is pending for approval"
+
+        },
+        {
+          "order_id": "#788234567890",
+          "date_of_order": "27th march,2018",
+          "total_amount": "1800",
+          "fruits_order_descriptiion": [
+            {
+              "fruit_name": "kela",
+              "number_of_kg_bought": "20",
+              "price_per_kg": "60"
+            }
+          ],
+          "OrderStatus": "Processing",
+          "OrderStatusDescription": "Your order is processing"
+        },
+        {
+          "order_id": "#108234567890",
+          "date_of_order": "27th march,2018",
+          "total_amount": "1800",
+          "fruits_order_descriptiion": [
+            {
+              "fruit_name": "kela",
+              "number_of_kg_bought": "20",
+              "price_per_kg": "60"
+            }
+          ],
+          "OrderStatus": "Cancelled",
+          "OrderStatusDescription": "Your have cancelled your order"
+        }
+      ]
+    }]
+  }
 }
