762cb917300668283646288a1523367bc7206af7
bantishaw <banti.shaw@outlook.com>
2018-04-01 21:22:42 +0530

    Added changes for select quanntity and enhanced the collection

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -11,7 +11,8 @@ import { Items } from '../../providers/providers';
 export class ItemDetailPage {
   item: any;
   keyValue: any;
-  numberOfItemsOrdered: any;
+  numberOfItemsOrdered: number;
+  totalAmount: number;
   constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
     this.item = navParams.get('item') || items.defaultItem;
     this.keyValue = Object.keys(this.item)[0];
@@ -20,19 +21,21 @@ export class ItemDetailPage {
 
   ionViewDidLoad() {
     this.numberOfItemsOrdered = 0;
+    this.totalAmount = 0;
   }
 
   decreaseQuantity(decreaseNumber) {
-    let intialCount = this.numberOfItemsOrdered;
     if (decreaseNumber) {
       this.numberOfItemsOrdered = this.numberOfItemsOrdered - 1;
+      this.totalAmount = this.totalAmount - this.item.rate;
     }
   }
 
   increaseQuantity(increaseNumber) {
     if (increaseNumber >= 0) {
       this.numberOfItemsOrdered = this.numberOfItemsOrdered + 1;
-    } 
+      this.totalAmount = this.totalAmount + this.item.rate;
+    }
   }
 }
 
