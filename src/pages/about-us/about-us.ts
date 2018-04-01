4879835063db5810cd828410194c3e83920fbfc9
bantishaw <sainaik786@gmail.com>
2018-04-02 01:23:34 +0530

    Added changes for about us page

--- src/pages/about-us/about-us.ts
+++ src/pages/about-us/about-us.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController, ActionSheetController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Api } from '../../providers/providers';
 import { ShowContentPage } from '../show-content/show-content';
+import { SocialSharing } from '@ionic-native/social-sharing';
 @IonicPage()
 @Component({
   selector: 'page-about-us',
@@ -11,8 +12,10 @@ import { ShowContentPage } from '../show-content/show-content';
 export class AboutUsPage {
   About_Page_Headings: any;
   displayAboutUs: any;
-  showFailureMessage : boolean = false;
+  FreshPoolLink: any;
   constructor(
+    private socialSharing: SocialSharing,
+    private actionSheetControler: ActionSheetController,
     public navCtrl: NavController, public navParams: NavParams,
     public apiProvider: Api, public toastCtrl: ToastController, public loadingCtrl: LoadingController,
     public http: Http) {
@@ -27,22 +30,82 @@ export class AboutUsPage {
     this.apiProvider.getAboutUsPage().then((result) => {
       this.About_Page_Headings = result;
       if (this.About_Page_Headings.response === "success") {
+        console.log(this.About_Page_Headings);
         loading.dismiss();
         setTimeout(() => {
           this.displayAboutUs = this.About_Page_Headings.data[0].aboutUsContents
-        }, 0)
-      } else{
-        loading.dismiss();
-        setTimeout(() => {
-          this.showFailureMessage = true;
         }, 0)
       }
     })
 
   }
-
   showContent(getPageInfo) {
-    this.navCtrl.push(ShowContentPage, { content: getPageInfo });
-  }
+    if (" ShareFreshPool" == getPageInfo.content) {
+      this.socialSharing.share(this.FreshPoolLink)
+        .then(() => {
 
-}
+        }).catch(() => {
+
+        });
+    }
+
+    else {
+      console.log(getPageInfo.content)
+      this.navCtrl.push(ShowContentPage, { content: getPageInfo });
+    }
+  }//showContent
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   showContent(getPageInfo) {
+
+//     console.log("David -1 " + getPageInfo.content);
+//     console.log("David -2 " + " ShareFreshPool");
+//     console.log("David-2 "+(" ShareFreshPool" == getPageInfo.content ));
+//     if (" ShareFreshPool" == getPageInfo.content) {
+//       console.log("David-3  " + getPageInfo.content);
+//       let ShareFreshPool = this.actionSheetControler.create({
+//         title: "Share Via",
+//         buttons: [
+
+//           {
+//             text: "Share on Facebook",
+//             icon: "logo-facebook"
+//           },
+//           {
+//             text: "Share on Twitter",
+//             icon: "logo-twitter"
+//           },
+//           {
+//             text: "Share on Whatsapp",
+//             icon: "logo-whatsapp"
+//           },
+//           {
+//             text: "Cancel",
+//             role: "destructive"
+//           }
+
+//         ]
+//       });
+//       ShareFreshPool.present();
+
+//     }
+//  else{
+//     console.log(getPageInfo.content)
+//     this.navCtrl.push(ShowContentPage, { content: getPageInfo });
+//   }}
+
+
