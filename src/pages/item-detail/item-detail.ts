899653934b862793af8544464a3c595706840fc2
bantishaw <banti.shaw@outlook.com>
2018-04-01 20:48:12 +0530

    Added changes for select quantity

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -10,10 +10,30 @@ import { Items } from '../../providers/providers';
 })
 export class ItemDetailPage {
   item: any;
-  keyValue : any
+  keyValue: any;
+  numberOfItemsOrdered: any;
   constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
     this.item = navParams.get('item') || items.defaultItem;
     this.keyValue = Object.keys(this.item)[0];
+
   }
-  
+
+  ionViewDidLoad() {
+    this.numberOfItemsOrdered = 0;
+  }
+
+  decreaseQuantity(decreaseNumber) {
+    let intialCount = this.numberOfItemsOrdered;
+    if (decreaseNumber) {
+      this.numberOfItemsOrdered = this.numberOfItemsOrdered - 1;
+    }
+  }
+
+  increaseQuantity(increaseNumber) {
+    if (increaseNumber >= 0) {
+      this.numberOfItemsOrdered = this.numberOfItemsOrdered + 1;
+    } 
+  }
 }
+
+
