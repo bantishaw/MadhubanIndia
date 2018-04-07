20f5a6d5bb358388669de6b1fafd39735dfdebd8
bantishaw <banti.shaw@outlook.com>
2018-04-08 03:35:02 +0530

    Added changes for shopping cart page

--- src/pages/shopping-cart/shopping-cart.ts
+++ src/pages/shopping-cart/shopping-cart.ts
@@ -11,6 +11,9 @@ import { Api } from '../../providers/providers';
 export class ShoppingCartPage {
   shoppingCartSegment : any;
   disableTab : Boolean = true;
+  productDisplay : any;
+  priceDisplay : any;
+  cartlength : any;
   constructor(public navCtrl: NavController, public navParams: NavParams,public apiProvider: Api) {
     this.shoppingCartSegment = "product";
   }
@@ -19,6 +22,10 @@ export class ShoppingCartPage {
     console.log('ionViewDidLoad ShoppingCartPage');
     console.log(this.apiProvider.shoppingCartData.data[0])
     this.disableTab = true;
+    this.priceDisplay = this.apiProvider.shoppingCartData.data[0].total_amount
+    this.productDisplay = this.apiProvider.shoppingCartData.data[0].order_descriptiion
+    this.cartlength = this.apiProvider.shoppingCartData.data[0].order_descriptiion.length
+    console.log(this.cartlength)
   }
 
 }
