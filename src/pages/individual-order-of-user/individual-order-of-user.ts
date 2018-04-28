a67ddf562a0182b2cdd6c4e2aabeaa173f10037a
bantishaw <banti.shaw@outlook.com>
2018-04-29 03:20:21 +0530

    Added changes for your order page

--- src/pages/individual-order-of-user/individual-order-of-user.ts
+++ src/pages/individual-order-of-user/individual-order-of-user.ts
@@ -8,16 +8,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IndividualOrderOfUserPage {
   OrderedindiVidualItemDetails: any;
-
-  ac:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.OrderedindiVidualItemDetails = navParams.get('content');
-    this.ac=navParams.get('name')
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad IndividualOrderOfUserPage');
-    console.log("IAmArun "+this.OrderedindiVidualItemDetails.date_of_order);
   }
 
 }
